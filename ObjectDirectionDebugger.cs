d2248e45a4a510e10e50d4266c93eb5037ea7431
adamjanik91 <a.janik334@gmail.com>
2017-11-01 17:22:43 +0100

    debugging helpers

---
+++ ObjectDirectionDebugger.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class ObjectDirectionDebugger : MonoBehaviour
+{
+    public float RayLength;
+    public Color ForwardRayColor;
+    public Color BackwardRayColor;
+    public Color RightRayColor;
+    public Color LeftRayColor;
+    public Color UpRayColor;
+    public Color DownRayColor;
+    public bool CastForwardRay;
+    public bool CastBackwardRay;
+    public bool CastRightRay;
+    public bool CastLeftRay;
+    public bool CastUpRay;
+    public bool CastDownRay;
+    void Update()
+    {
+        CastDirectionRays();
+    }
+
+    private void CastDirectionRays()
+    {
+        if (CastForwardRay)
+        {
+            Vector3 forward = transform.TransformDirection(Vector3.forward) * RayLength;
+            Debug.DrawRay(transform.position, forward, ForwardRayColor);
+        }
+        if (CastBackwardRay)
+        {
+            Vector3 backward = transform.TransformDirection(Vector3.back) * RayLength;
+            Debug.DrawRay(transform.position, backward, BackwardRayColor);
+        }
+        if (CastRightRay)
+        {
+            Vector3 right = transform.TransformDirection(Vector3.right) * RayLength;
+            Debug.DrawRay(transform.position, right, RightRayColor);
+        }
+        if (CastLeftRay)
+        {
+            Vector3 left = transform.TransformDirection(Vector3.left) * RayLength;
+            Debug.DrawRay(transform.position, left, LeftRayColor);
+        }
+        if (CastUpRay)
+        {
+            Vector3 up = transform.TransformDirection(Vector3.up) * RayLength;
+            Debug.DrawRay(transform.position, up, UpRayColor);
+        }
+        if (CastDownRay)
+        {
+            Vector3 down = transform.TransformDirection(Vector3.down) * RayLength;
+            Debug.DrawRay(transform.position, down, DownRayColor);
+        }
+    }
+}
---
+++ ObjectMovementInfoProvider.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ObjectMovementInfoProvider : MonoBehaviour
+{
+    public bool ShowVelocity;
+
+    private Rigidbody _Rigidbody;
+    private void Start()
+    {
+        _Rigidbody = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        if (ShowVelocity) {
+            Vector3 currentVelocity = _Rigidbody.velocity;
+            Debug.Log(currentVelocity.normalized);
+        }
+    }
+}
